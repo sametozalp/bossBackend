8c7649b6c4d1125402ff294108d0f5c062377454
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2025-06-18 18:45:33 +0300

    Initial commit

---
+++ src/main/java/com/boss/bossBackend/BossBackendApplication.java
@@ -0,0 +1,12 @@
+package com.boss.bossBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BossBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BossBackendApplication.class, args);
+	}
+}
---
+++ src/test/java/com/boss/bossBackend/BossBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boss.bossBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BossBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
