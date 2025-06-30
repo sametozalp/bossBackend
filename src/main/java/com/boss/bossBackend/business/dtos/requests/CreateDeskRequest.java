179acc65c72d78358a01e06dbece29d8500187be
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2025-06-30 22:49:40 +0300

    add notblank annotation to requests

--- src/main/java/com/boss/bossBackend/business/dtos/requests/CreateDeskRequest.java
+++ src/main/java/com/boss/bossBackend/business/dtos/requests/CreateDeskRequest.java
@@ -1,7 +1,10 @@
 package com.boss.bossBackend.business.dtos.requests;
 
+import jakarta.validation.constraints.NotBlank;
+
 public class CreateDeskRequest {
 
+    @NotBlank(message = "Room id cannot be blank")
     private String roomId;
 
     public String getRoomId() {
--- src/main/java/com/boss/bossBackend/business/dtos/requests/CreateRoomRequest.java
+++ src/main/java/com/boss/bossBackend/business/dtos/requests/CreateRoomRequest.java
@@ -1,7 +1,10 @@
 package com.boss.bossBackend.business.dtos.requests;
 
+import jakarta.validation.constraints.NotBlank;
+
 public class CreateRoomRequest {
 
+    @NotBlank(message = "Technopark user id cannot be blank")
     private String technoparkUserId;
 
     public String getTechnoparkUserId() {
