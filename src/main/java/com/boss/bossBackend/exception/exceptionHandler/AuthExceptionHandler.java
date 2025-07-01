9416ae66cb7c3d3210bbb1b762cb95a3679a910a
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2025-07-01 23:02:49 +0300

    Create AuthExceptionHandler.java

---
+++ src/main/java/com/boss/bossBackend/exception/exceptionHandler/AuthExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.boss.bossBackend.exception.exceptionHandler;
+
+import com.boss.bossBackend.common.utilities.results.ErrorResult;
+import com.boss.bossBackend.exception.authException.LoginException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class AuthExceptionHandler {
+
+    @ExceptionHandler(LoginException.class)
+    public ResponseEntity<?> loginException(LoginException exception) {
+        return new ResponseEntity<>(new ErrorResult(exception.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+}
