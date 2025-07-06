621d31e3416bda9b58479efef49155ddb6eb920d
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2025-07-06 12:47:49 +0300

    Update CustomerAccountController.java

--- src/main/java/com/boss/bossBackend/api/controllers/CustomerAccountController.java
+++ src/main/java/com/boss/bossBackend/api/controllers/CustomerAccountController.java
@@ -3,10 +3,7 @@ package com.boss.bossBackend.api.controllers;
 import com.boss.bossBackend.business.abstracts.CustomerAccountService;
 import com.boss.bossBackend.entities.enums.ApprovalStatusEnum;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/customerAccount")
@@ -23,7 +20,7 @@ public class CustomerAccountController {
         return ResponseEntity.ok(customerAccountService.getAccountsByApprovalStatusSortedByCreatedDate(approvalStatusEnum, associatedTechnoparkId));
     }
 
-    @PostMapping("/changeApprovalStatus")
+    @PutMapping("/changeApprovalStatus")
     ResponseEntity<?> changeApprovalStatus(@RequestParam String customerId, @RequestParam ApprovalStatusEnum approvalStatusEnum) {
         return ResponseEntity.ok(customerAccountService.changeApprovalStatus(customerId, approvalStatusEnum));
     }
